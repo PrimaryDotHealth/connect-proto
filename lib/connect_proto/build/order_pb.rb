237e64c4f46a682b51c63b35d5ea4e0dcd470c3d
Ruy Diaz <diazruy@users.noreply.github.com>
2022-08-04 14:32:14 -0700

    Add new STATUS_REPORTED status for Orders

--- lib/connect_proto/build/order_pb.rb
+++ lib/connect_proto/build/order_pb.rb
@@ -82,6 +82,7 @@ Google::Protobuf::DescriptorPool.generated_pool.build do
       value :STATUS_CANCEL, 2
       value :STATUS_RESULTED, 3
       value :STATUS_NEW, 4
+      value :STATUS_REPORTED, 5
     end
     add_enum "primary.connect.Order.Order.Priority" do
       value :PRIORITY_UNKNOWN, 0
